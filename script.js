7b730f65c15438ff64fa55d81168e4284631b4e6
debugger-com <83845510+debugger-com@users.noreply.github.com>
2023-07-22 22:10:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -27,8 +27,33 @@ const purchaseBox = () => {
   }
 };
 
+const saveData = () => {
+  const data = {
+    balance: flopMoneyBalance,
+    boxesPurchased: boxesPurchased
+  };
+  localStorage.setItem('flopMoneyData', JSON.stringify(data));
+  alert('Data saved successfully!');
+};
+
+const loadData = () => {
+  const savedData = localStorage.getItem('flopMoneyData');
+  if (savedData) {
+    const data = JSON.parse(savedData);
+    flopMoneyBalance = data.balance;
+    boxesPurchased = data.boxesPurchased;
+    updateBalance();
+    updateBoxesPurchased();
+    alert('Data loaded successfully!');
+  } else {
+    alert('No saved data found!');
+  }
+};
+
 document.getElementById('workButton').addEventListener('click', workForFlopMoney);
 document.getElementById('purchaseButton').addEventListener('click', purchaseBox);
+document.getElementById('saveButton').addEventListener('click', saveData);
+document.getElementById('loadButton').addEventListener('click', loadData);
 
 // Initial updates
 updateBalance();
